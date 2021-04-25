422c9a440fd889aca65b441493eaefb89be8110e
Pietro Barbiero <barbiero@tutanota.com>
2021-04-25 11:35:16 +0200

    Concept awareness loss

--- torch_explain/nn/functional/loss.py
+++ torch_explain/nn/functional/loss.py
@@ -1,12 +1,21 @@
 import torch
 
-from torch_explain.nn.logic import LogicAttention
+from torch_explain.nn.logic import ConceptAwareness
 
 
 def l1_loss(model: torch.nn.Module):
     loss = 0
     for module in model.children():
-        if isinstance(module, LogicAttention) and module.shrink:
+        if isinstance(module, ConceptAwareness) and module.shrink:
+            loss += torch.norm(module.weight, 1)
+            break
+    return loss
+
+
+def concept_awareness_loss(model: torch.nn.Module):
+    loss = 0
+    for module in model.children():
+        if isinstance(module, ConceptAwareness) and module.shrink:
             loss += torch.norm(module.beta, 1)
             break
     return loss
@@ -16,7 +25,7 @@ def whitening_loss(model: torch.nn.Module, device: torch.device = torch.device('
     loss = 0
     cov = None
     for module in model.children():
-        if isinstance(module, LogicAttention):
+        if isinstance(module, ConceptAwareness):
             # the target covariance matrix is diagonal
             n_concepts = module.conceptizator.concepts.shape[1]
             cov_objective = torch.eye(n_concepts).to(device)
