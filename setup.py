482db81d9925e7a2304d4c848930a2ed41daea32
Mateo Espinosa Zarlenga <mateoespinosaz2@gmail.com>
2022-01-11 11:52:18 +0000

    Adding correct missing dependency version in setup.py.

--- setup.py
+++ setup.py
@@ -21,21 +21,31 @@ URL = 'https://github.com/pietrobarbiero/pytorch_explain'
 LICENSE = 'Apache 2.0'
 DOWNLOAD_URL = 'https://github.com/pietrobarbiero/pytorch_explain'
 VERSION = __version__
-INSTALL_REQUIRES = ['numpy', 'scipy', 'scikit-learn', 'pandas', 'torch', 'sympy', 'pytorch_lightning']
-CLASSIFIERS = ['Intended Audience :: Science/Research',
-               'Intended Audience :: Developers',
-               'License :: OSI Approved',
-               'Programming Language :: Python',
-               'Topic :: Software Development',
-               'Topic :: Scientific/Engineering',
-               'Operating System :: Microsoft :: Windows',
-               'Operating System :: POSIX',
-               'Operating System :: Unix',
-               'Operating System :: MacOS',
-               'Programming Language :: Python :: 2.7',
-               'Programming Language :: Python :: 3.5',
-               'Programming Language :: Python :: 3.6',
-               'Programming Language :: Python :: 3.7']
+INSTALL_REQUIRES = [
+    'numpy',
+    'scipy',
+    'scikit-learn',
+    'pandas',
+    'torch>=1.9',
+    'sympy',
+    'pytorch_lightning',
+]
+CLASSIFIERS = [
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved',
+    'Programming Language :: Python',
+    'Topic :: Software Development',
+    'Topic :: Scientific/Engineering',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Operating System :: MacOS',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+]
 EXTRAS_REQUIRE = {
     'tests': [
         'pytest',
@@ -49,18 +59,20 @@ EXTRAS_REQUIRE = {
     ]
 }
 
-setup(name=DISTNAME,
-      maintainer=MAINTAINER,
-      maintainer_email=MAINTAINER_EMAIL,
-      description=DESCRIPTION,
-      license=LICENSE,
-      url=URL,
-      version=VERSION,
-      download_url=DOWNLOAD_URL,
-      long_description=LONG_DESCRIPTION,
-      long_description_content_type='text/x-rst',
-      zip_safe=False,  # the package can run out of an .egg file
-      classifiers=CLASSIFIERS,
-      packages=find_packages(),
-      install_requires=INSTALL_REQUIRES,
-      extras_require=EXTRAS_REQUIRE)
+setup(
+    name=DISTNAME,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    description=DESCRIPTION,
+    license=LICENSE,
+    url=URL,
+    version=VERSION,
+    download_url=DOWNLOAD_URL,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/x-rst',
+    zip_safe=False,  # the package can run out of an .egg file
+    classifiers=CLASSIFIERS,
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+)
