964a37c54d064ff9844b6ea441dda302e5b0aaae
Pietro Barbiero <barbiero@tutanota.com>
2022-04-20 13:20:44 +0100

    Add concept homogeneity metric

--- torch_explain/logic/metrics.py
+++ torch_explain/logic/metrics.py
@@ -5,7 +5,7 @@ import sympy
 import torch
 import numpy as np
 from scipy.spatial.distance import cdist
-from sklearn.metrics import f1_score, accuracy_score
+from sklearn.metrics import f1_score, accuracy_score, homogeneity_score
 from sklearn_extra.cluster import KMedoids
 from sympy import to_dnf, lambdify
 
@@ -35,6 +35,50 @@ def auc_truth_table_score(c_vec: np.array, y_pred: np.array, y_test: np.array,
     max_auc = np.trapz(np.ones(len(n_clusters)))
     auc_score = np.trapz(accuracies) / max_auc
     return auc_score, n_clusters, np.array(accuracy_list)
+
+
+def embedding_homogeneity(c_vec: np.array, c_test: np.array, y_test: np.array, step: int) -> [float, float]:
+    """
+    Computes the alignment between learnt concepts and labels.
+
+    :param c_vec: predicted concept representations (can be concept embeddings)
+    :param c_test: concept ground truth labels
+    :param y_test: task ground truth labels
+    :param step: integration step
+    :return: concept alignment AUC, task alignment AUC
+    """
+    # compute the maximum value for the AUC
+    n_clusters = np.arange(2, len(c_vec), step)
+    max_auc = np.trapz(np.ones(len(n_clusters)))
+
+    # for each concept:
+    #   1. find clusters
+    #   2. compare cluster assignments with ground truth concept/task labels
+    concept_auc, task_auc = [], []
+    for concept_id in range(c_test.shape[1]):
+        concept_homogeneity, task_homogeneity = [], []
+        for nc in n_clusters:
+            # train clustering algorithm
+            kmedoids = KMedoids(n_clusters=nc, random_state=0)
+            if c_vec.shape[1] != c_test.shape[1]:
+                c_cluster_labels = kmedoids.fit_predict(c_vec)
+            elif c_vec.shape[1] == c_test.shape[1] and len(c_vec.shape) == 2:
+                c_cluster_labels = kmedoids.fit_predict(c_vec[:, concept_id].reshape(-1, 1))
+            else:
+                c_cluster_labels = kmedoids.fit_predict(c_vec[:, concept_id])
+
+            # compute alignment with ground truth labels
+            concept_homogeneity.append(homogeneity_score(c_test[:, concept_id], c_cluster_labels))
+            task_homogeneity.append(homogeneity_score(y_test, c_cluster_labels))
+
+        # compute the area under the curve
+        concept_auc.append(np.trapz(np.array(concept_homogeneity)) / max_auc)
+        task_auc.append(np.trapz(np.array(task_homogeneity)) / max_auc)
+
+    # return the average alignment across all concepts
+    concept_auc = np.mean(concept_auc)
+    task_auc = np.mean(task_auc)
+    return concept_auc, task_auc
 
 
 def test_explanation(formula: str, x: torch.Tensor, y: torch.Tensor, target_class: int):
